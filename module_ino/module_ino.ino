9394ded4f197d182f46bd3f9d3e5332b453d76ec
SpiritWhite66 <vincent.guillamot@laposte.net>
2015-04-12 16:02:53 +0200

    Update module_ino.ino
    Ajout de la communication série

--- module_ino/module_ino.ino
+++ module_ino/module_ino.ino
@@ -10,21 +10,18 @@ const int led = 3;
 const int relay = 4;
  
 int etatBouton;
-boolean etat, statu;
+boolean etat, statu,access;
 
 void setup(){
-  
+  Serial.begin(115200);
   /*** Initialisation de la connexion sans-fil à base de NRF24L01 ***/
   /*** VERSION DE TEST
    ***Absence du code pour vérifier que celui ci fonctionne ...
    
-   Serial.begin(9600);
-
   Mirf.cePin = 8; // CE
   Mirf.csnPin = 7; // CSN
   Mirf.spi = &MirfHardwareSpi; 
   Mirf.init(); 
-
   Mirf.channel = 42; // On utilise le canal 42 parceque c'est fun ... 
   Mirf.payload = sizeof(unsigned long); //ici il faut déclarer la taille du "payload" soit du message qu'on va transmettre, au max 32 octets
   Mirf.config(); //on configure ce joyeux bordel :)
@@ -38,8 +35,9 @@ void setup(){
   pinMode(led, OUTPUT);
   pinMode(bouton, INPUT); 
   etatBouton = HIGH; 
-  etat = false; 
-  statu = false;
+  etat=true;
+  statu=true;
+  access=false; //jeton d'accès de la connexion sans fils
   
 }
  
@@ -65,28 +63,55 @@ void loop(){
     
     Serial.println("Change l'état du Relai");
   }*/
-  
-  
-  if(digitalRead(bouton)==HIGH and statu == false ) // bouton poussé, on rentre uniquement à la première detection de l'appui
-  { 
-    if(etat == true)
-    {
-      etat = false;
-      statu = true; 
-      digitalWrite(relay, LOW);
-      digitalWrite(led, LOW);  
-    }
-    if(etat == false)
-    {
-      etat = true; 
-      statu = true; 
-      digitalWrite(relay, HIGH);
-      digitalWrite(led, HIGH); 
-    }
-  }
-  else if(digitalRead(bouton)==LOW and statu == true) // cette ligne permet de bloqué le changement d'état du relay x fois quand on appui sur le bouton 
-  {
-    statu = false;
-  }
+ /*if(!Mirf.isSending() && Mirf.dataReady())
+ {
+   Mirf.getData(data); // on récupére le message
+   access=true; //j'active le jeton
+   statu = true
+ }*/
+      char data = 0; //variable contenant le caractère à lire
+      int reception = 0; //variable contenant le nombre de caractère disponibles dans le buffer
     
+      reception = Serial.available();
+    
+      while(reception > 0) //tant qu'il y a des caractères à lire
+      {
+          access = true;
+          data = Serial.read(); //on lit le caractère
+          reception = Serial.available(); //on relit le nombre de caractères dispo
+      }
+      Serial.println(statu); 
+      if(digitalRead(bouton)==LOW or access==true) // bouton poussé, on rentre uniquement à la première detection de l'appui
+      {
+      
+        if(statu==true)
+        {
+          if((etat == true or data=='1')and data!='0')
+          {
+            Serial.println("j'allume"); 
+            digitalWrite(relay, HIGH);
+            digitalWrite(led, LOW);
+            etat=false;
+          }
+          else if(etat==false or data=='0')
+          {
+            Serial.println("j'éteins"); 
+            digitalWrite(relay, LOW);
+            digitalWrite(led, HIGH);
+            etat=true;
+          }
+                      
+        }
+        statu=false;   //je bloque les actions tant que le bouton n'a pas était relaché
+        access=false; //on rebloque l'accès par la connexion sans fils
+       }
+     
+     else
+     {
+       statu = true; 
+     }
+    
+    //fin du programme
+ 
+ 
 }
